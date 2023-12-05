466a33f5bef06e12b5e8f2956d3b764b658024b7
Nahom - TG <46280184+nahomtekeste@users.noreply.github.com>
2023-12-05 16:31:03 +0000

    Update jenkinsfile.java

--- jenkinsfile.java
+++ jenkinsfile.java
@@ -2,6 +2,20 @@ node {
   def app
 
     stage('detetct github changes'){
-
+        echo 'detecting repo changes...'
+        checkout scm
+    }
+  stage ('build docker image'){
+    echo 'building the application... '
+    app = docker.build('nahomtekeste/coursework2')
   }
+  stage('push image to docker') {
+    echo 'pushing the application to dockerhub...'
+    docker.withRegistry'https://registry.hub.docker.com' , 'docker-hub-credintials'){
+    app.push("${env.BUILD_NUMBER}")
+      app.push('latest')
+    }
+     
+  }
+  
 }
